d9dee00fbe4d4fb1116de990680501cc54ecdc8f
Saulo Aguiar <sauloaguiar89@gmail.com>
2017-08-10 11:33:56 -0300

    Finishes redux with axios request sample
    Introduced the concept of a Container - a component connected to the redux state
    Introduced middleware - ReduxPromise - unwrapped the promise created by axios
    Introduced http calls - Axios - that uses promises to grab online data
    Avoided mutations in reducer by always returning new objects
    Some ES6 syntax - descruturing and spread

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'a8cffccefc8a2ffa590292953c745d25';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
